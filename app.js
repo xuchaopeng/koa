3e84a543c8320746bf7bfd46797e4b72e159bab9
m18770074875_1@163.com <m18770074875_1@163.com>
2019-08-11 23:23:42 +0800

    初始化

---
+++ app.js
@@ -0,0 +1,70 @@
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+
+const session = require('koa-generic-session');
+const Redis = require('koa-redis');
+
+const mongoose = require('mongoose');
+const dbConfig = require('./dbs/config');
+
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+// error handler
+onerror(app);
+
+//redis
+app.keys = ['keys', 'keyskeys'];
+app.use(
+  session({
+    store: new Redis()
+  })
+);
+
+// middlewares
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text']
+  })
+);
+app.use(json());
+app.use(logger());
+app.use(require('koa-static')(__dirname + '/public'));
+
+app.use(
+  views(__dirname + '/views', {
+    extension: 'ejs'
+  })
+);
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+});
+
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
+
+//dbs
+mongoose.connect(
+  dbConfig.dbs,
+  {
+    useNewUrlParser: true
+  }
+);
+
+// error-handling
+app.on('error', (err, ctx) => {
+  console.error('server error', err, ctx);
+});
+
+module.exports = app;
